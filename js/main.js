6c483d2004d7a7a7084a7430f49ec8b0524286ae
Jamie Hodgson <jamie-hodgson@live.com>
2023-05-18 15:13:55 +0200

    hero

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+
+// Mobile menu
+const $nav = document.getElementById("nav");
+
+function toggleNav() {
+    $nav.classList.toggle("open");
+}
+
+function closeNav() {
+    $nav.classList.remove("open");
+}
+
+const $openMenu = document.getElementById("open-menu");
+const $closeMenu = document.getElementById("close-menu");
+
+$openMenu.onclick = toggleNav;
+$closeMenu.onclick = toggleNav;
