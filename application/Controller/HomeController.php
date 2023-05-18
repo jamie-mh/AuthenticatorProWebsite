ccce92c4c1fbe0f2e2579dfcabbc994a1c4e5bb6
Jamie Hodgson <jamie-hodgson@live.com>
2023-05-18 23:13:42 +0200

    home meta

--- application/Controller/HomeController.php
+++ application/Controller/HomeController.php
@@ -11,8 +11,8 @@ class HomeController extends Controller
     public function index(): Response
     {
         $res = new PageResponse();
-        $res->meta->title = "test";
-        $res->meta->description = "test";
+        $res->meta->title = "Free and open-source two-factor authentication app";
+        $res->meta->description = "Authenticator Pro is a free and open-source two-factor authentication app for Android. It features encrypted backups, icons, categories, a high level of customisation and even a Wear OS app.";
         $res->setView("home/index.twig");
         return $res;
     }
