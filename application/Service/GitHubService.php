7217bba7ee8e5cefe2189b04a1f5b985a194d076
Jamie Hodgson <jamie-hodgson@live.com>
2023-07-14 16:58:54 +0200

    license headers

--- application/Service/GitHubService.php
+++ application/Service/GitHubService.php
@@ -1,4 +1,6 @@
 <?php
+// Copyright (C) 2023 jmh
+// SPDX-License-Identifier: GPL-3.0-only
 
 namespace AuthPro\Service;
 
--- public/index.php
+++ public/index.php
@@ -1,4 +1,6 @@
 <?php
+// Copyright (C) 2023 jmh
+// SPDX-License-Identifier: GPL-3.0-only
 
 use AuthPro\Core\Application;
 use Dotenv\Dotenv;
