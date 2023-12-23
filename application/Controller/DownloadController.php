3f4a8e2bb779d5ddc5f64c2add6e70c0ae69f9b2
Jamie Hodgson <jamie-hodgson@live.com>
2023-12-23 13:11:42 +0100

    add apk link file to downloads page

--- application/Controller/DownloadController.php
+++ application/Controller/DownloadController.php
@@ -7,15 +7,26 @@ namespace AuthPro\Controller;
 use AuthPro\Core\Controller;
 use AuthPro\Core\Response;
 use AuthPro\Core\Response\PageResponse;
+use AuthPro\Service\GitHubService;
 
 class DownloadController extends Controller
 {
+    private readonly GitHubService $gitHubService;
+
+    public function __construct()
+    {
+        $this->gitHubService = new GitHubService();
+    }
+
     public function index(): Response
     {
+        $latestTag = $this->gitHubService->getLatestReleaseTag();
+
         $res = new PageResponse();
         $res->meta->title = "Download";
-        $res->meta->description = "Download the Authenticator Pro app for free";
+        $res->meta->description = "Download the open-source Authenticator Pro app for free";
         $res->setView("download/index.twig");
+        $res->viewData = ["latestTag" => $latestTag];
         return $res;
     }
 }
--- application/Service/GitHubService.php
+++ application/Service/GitHubService.php
@@ -4,12 +4,19 @@
 
 namespace AuthPro\Service;
 
-use AuthPro\Service\Exception\GitHubException;
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use JsonException;
 use Redis;
 use RedisException;
 
 readonly class GitHubService
 {
+    private const WIKI_FILE_URL = "https://raw.githubusercontent.com/wiki/jamie-mh/AuthenticatorPro";
+    private const MARKDOWN_FILE_URL = "https://raw.githubusercontent.com/jamie-mh/AuthenticatorPro/master";
+    private const RELEASE_INFO_URL = "https://api.github.com/repos/jamie-mh/AuthenticatorPro/releases/latest";
+
+    private Client $client;
     private Redis $redis;
 
     /**
@@ -17,34 +24,35 @@ readonly class GitHubService
      */
     public function __construct()
     {
+        $this->client = new Client();
         $this->redis = new Redis();
         $this->redis->connect(REDIS_HOST, REDIS_PORT);
     }
 
     /**
      * @throws RedisException
-     * @throws GitHubException
+     * @throws GuzzleException
      */
     public function getWikiPage(string $fileName): string
     {
-        $url = "https://raw.githubusercontent.com/wiki/jamie-mh/AuthenticatorPro/$fileName";
+        $url = self::WIKI_FILE_URL . "/$fileName";
         return $this->getMarkdownFile($url);
     }
 
     /**
      * @throws RedisException
-     * @throws GitHubException
+     * @throws GuzzleException
      */
     public function getMarkdownPage(string $filePath): string
     {
-        $url = "https://raw.githubusercontent.com/jamie-mh/AuthenticatorPro/master/$filePath";
+        $url = self::MARKDOWN_FILE_URL . "/$filePath";
         $content = $this->getMarkdownFile($url);
         return preg_replace("/^# (.*)$/m", "", $content);
     }
 
     /**
      * @throws RedisException
-     * @throws GitHubException
+     * @throws GuzzleException
      */
     private function getMarkdownFile(string $url)
     {
@@ -52,16 +60,33 @@ readonly class GitHubService
         $markdown = $this->redis->get($key);
 
         if ($markdown === false) {
-            $markdown = file_get_contents($url);
-
-            if ($markdown === false) {
-                throw new GitHubException("Failed to load markdown file");
-            }
-
+            $res = $this->client->get($url);
+            $markdown = (string) $res->getBody();
             $this->redis->set($key, $markdown);
             $this->redis->expire($key, 86400);
         }
 
         return $markdown;
+    }
+
+    /**
+     * @throws RedisException
+     * @throws JsonException
+     */
+    public function getLatestReleaseTag(): string
+    {
+        $key = "github:latest_release_tag";
+        $tag = $this->redis->get($key);
+
+        if ($tag === false) {
+            $res = $this->client->get(self::RELEASE_INFO_URL);
+            $releaseInfo = json_decode((string) $res->getBody(), false, 512, JSON_THROW_ON_ERROR);
+            $tag = $releaseInfo->tag_name;
+
+            $this->redis->set($key, $tag);
+            $this->redis->expire($key, 86400);
+        }
+
+        return $tag;
     }
 }
